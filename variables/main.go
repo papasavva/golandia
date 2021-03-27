14aaffc7251643afc5de9fbd882b4c56f559d9cb
papasavvas <papasavva@protonmail.com>
2021-03-27 12:07:58 +0100

    Variable string with shorthand assignment

--- variables/main.go
+++ variables/main.go
@@ -11,4 +11,16 @@ func main() {
 
 	var temperature int8 = -128
 	fmt.Println("temperature overflow: ", temperature-1)
+
+	//boolean
+	var isLocked bool = false
+	fmt.Println("front door is locked: ", isLocked)
+
+	//String
+	var favouriteRodent string = "hamster"
+	fmt.Println("My favourite rodent is a ", favouriteRodent)
+
+	//shorthand assignment statement in place of a var declaration with implicit type
+	rodentSubFamily := "Cricetidae"
+	fmt.Println("Rodent subfamilty: ", rodentSubFamily)
 }
