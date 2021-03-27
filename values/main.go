93fdad955d09c067df3ad2d74ef49a630d6ce1f3
papasavvas <papasavva@protonmail.com>
2021-03-27 11:37:22 +0100

    Values: integers, floats, strings and booleans

---
+++ values/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go" + "lang")
+	fmt.Println("1" + "2")
+	fmt.Println(10 / 2)
+	fmt.Println(10.5 / 2.25)
+	fmt.Println(true && false)
+	fmt.Println(!false && true)
+}
