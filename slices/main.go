ac0e7125cdf8026a0a60fafc0c17f76191f30fb7
papasavvas <papasavva@protonmail.com>
2021-03-30 23:09:38 +0200

    Use slice operator and copy function

--- slices/main.go
+++ slices/main.go
@@ -10,4 +10,14 @@ func main() {
 	for i := 0; i < len(grades); i++ {
 		fmt.Printf("Grade %d is %s\n", i, grades[i])
 	}
+
+	//lets copy a slice
+	var uniqueGrades = make([]string, len(grades))
+	copy(uniqueGrades, grades)
+
+	fmt.Println(uniqueGrades)
+
+	//use slice operator [low:high]
+	var selectedGrades = grades[1:3]
+	fmt.Println(selectedGrades)
 }
