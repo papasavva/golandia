a4a5a4605211103ca4909bcf9d4ae3f0bd487ece
papasavvas <papasavva@protonmail.com>
2021-04-01 19:50:20 +0200

    Added map to range

--- range/main.go
+++ range/main.go
@@ -7,6 +7,15 @@ func main() {
 	var luckyNumbers = []int8{7, 4, 9}
 
 	for index, value := range luckyNumbers {
-		fmt.Println("Index: ", index, " and Value: ", value)
+		fmt.Println("Index:", index, " and Value:", value)
+	}
+
+	var colours = make(map[string]string)
+	colours["red"] = "#FF0000"
+	colours["blue"] = "#0000FF"
+	colours["yellow"] = "#FFFF00"
+
+	for index, value := range colours {
+		fmt.Println("Colour:", index, " and Hex Code:", value)
 	}
 }
