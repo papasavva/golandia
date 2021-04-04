211f4dd272fdc64d98af7c9430130758efbfc164
papasavvas <papasavva@protonmail.com>
2021-04-04 12:37:13 +0200

    Make code shorter for fibonacci function

--- recursion/main.go
+++ recursion/main.go
@@ -3,15 +3,13 @@ package main
 import "fmt"
 
 func fibonacci(n int) int {
-	if n == 0 {
-		return 0
+	if n == 0 || n == 1 {
+		return n
 	}
-	if n == 1 {
-		return 1
-	}
+
 	return fibonacci(n-1) + fibonacci(n-2)
 }
 func main() {
-	const number = 10
+	const number = 6
 	fmt.Println("Fibonacci of ", number, ":", fibonacci(number))
 }
