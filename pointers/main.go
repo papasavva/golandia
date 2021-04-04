8622663ffe7b5ea964862d4e8fcf6b9fc6bfa389
papasavvas <papasavva@protonmail.com>
2021-04-04 15:55:13 +0200

    Pointer on return value

--- pointers/main.go
+++ pointers/main.go
@@ -5,8 +5,17 @@ import "fmt"
 func double(number *int) {
 	*number *= 2
 }
+
+func hello() *int {
+	i := 5
+	return &i
+}
+
 func main() {
 	x := 5
 	double(&x)
 	fmt.Println("Double of", x, "is ", x)
+
+	d := hello()
+	fmt.Println("Value of d", *d)
 }
