4b834d3a33cf05991c3fee6e8bffd912db0457b2
papasavvas <papasavva@protonmail.com>
2021-04-04 18:48:27 +0200

    Polymorphism with interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Shape interface {
 	Area() float64
@@ -16,6 +19,7 @@ type Circle struct {
 	radius float64
 }
 
+//Rectangle
 func (r Rectangle) Area() float64 {
 	return r.height * r.width
 }
@@ -24,12 +28,26 @@ func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.height + r.width)
 }
 
+//Circle
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
 func main() {
 	var shape1 Shape = Rectangle{
 		width:  5,
 		height: 10,
 	}
 
+	var shape2 Shape = Circle{5}
+
 	fmt.Println("Rectangle area:", shape1.Area())
 	fmt.Println("Rectangle perimeter:", shape1.Perimeter())
+
+	fmt.Println("Circle area:", shape2.Area())
+	fmt.Println("Circle perimeter:", shape2.Perimeter())
 }
